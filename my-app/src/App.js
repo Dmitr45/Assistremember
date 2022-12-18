ddb3c104e00e9e5879594e51b9313101a144efeb
DMITRY PLETNEV <Dmitr45@yandex.ru>
2022-12-18 19:09:10 +0700

    Инициализируем как telegram приложение

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,29 @@
+import { useEffect } from 'react';
 import './App.css';
 import ReactHeader from  './components/header/ReactHeader';
 import TimeEvent from './components/input/TimeEvent';
 //import axios from 'axios';
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+const onClose = () =>{    // Функция закрытия телеграм-приложения
+  console.log('Закрыть веб приложение');
+  tg.close();
+}
+
+useEffect(()=>{
+tg.ready(); // Метод сообщает, что приложение полностью проинициализировалость и его можно отрисовывать
+}, [])
+
+
   return (
     <div>
     <ReactHeader title="AssistRemember"/>
     <main className='App-content _container'>
-      < TimeEvent/>
+    { /*  < TimeEvent/> */}
+    <button onClick={onClose}>Закрыть</button>
     </main>
-
     </div>
   );
 }
